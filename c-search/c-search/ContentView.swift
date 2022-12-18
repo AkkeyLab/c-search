45d9e82bd5cfafa592ba8998924a20028ba8960a
AKIO <17640999+AkkeyLab@users.noreply.github.com>
2022-12-19 01:18:38 +0900

    :sparkles: Create search text input function for iOS

--- c-search/c-search/ContentView.swift
+++ c-search/c-search/ContentView.swift
@@ -2,19 +2,35 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-    @State private var corporate = ""
+    @State private var responseText = ""
+    @State private var searchText = ""
 
     var body: some View {
-        Text(corporate)
-            .onAppear() {
-                load()
+        VStack {
+            Text(responseText)
+            Spacer()
+            HStack {
+                TextField(
+                    "Enter corporate name",
+                    text: $searchText
+                )
+                .textFieldStyle(.roundedBorder)
+                Button(action: { load() }) {
+                    Label("Search", systemImage: "magnifyingglass")
+                }
+                .buttonStyle(.bordered)
             }
+        }
+        .padding()
     }
 
     private func load() {
-        SearchCorporate().search(name: "ＡｋｋｅｙＬａｂ") { result, _ in
-            guard let corporate = result else { return }
-            self.corporate = corporate
+        guard !searchText.isEmpty,
+              let fullWidthString = searchText.applyingTransform(.fullwidthToHalfwidth, reverse: true)
+        else { return }
+        SearchCorporate().search(name: fullWidthString) { result, _ in
+            guard let responseText = result else { return }
+            self.responseText = responseText
         }
     }
 }
