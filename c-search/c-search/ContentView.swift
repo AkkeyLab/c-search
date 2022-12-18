969d1f0bff5918d7c75fada1e403ed3f6a4dc10d
AKIO <17640999+AkkeyLab@users.noreply.github.com>
2022-12-19 01:30:31 +0900

    :sparkles: Add status view process for iOS

--- c-search/c-search/ContentView.swift
+++ c-search/c-search/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-    @State private var responseText = ""
+    @State private var responseText = "No search result"
     @State private var searchText = ""
 
     var body: some View {
@@ -28,8 +28,13 @@ struct ContentView: View {
         guard !searchText.isEmpty,
               let fullWidthString = searchText.applyingTransform(.fullwidthToHalfwidth, reverse: true)
         else { return }
-        SearchCorporate().search(name: fullWidthString) { result, _ in
-            guard let responseText = result else { return }
+        responseText = "Now Loading"
+        let networkError: () -> Void = {
+            responseText = "Network Error"
+        }
+        SearchCorporate().search(name: fullWidthString) { result, error in
+            guard error == nil else { return networkError() }
+            guard let responseText = result else { return networkError() }
             self.responseText = responseText
         }
     }
